b37734a55de1446883f71238f5b4e8c3768f4fc1
SwapnilMk <mswapnil218@gmail.com>
2025-09-13 02:21:44 +0530

    homepage completed!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import SiteFooter from "./components/footer/site-footer";
+import SiteBanner from "./components/header/header-banner";
 import SiteHeader from "./components/header/site-header";
+import HomeSlider from "./components/home-slider";
+import EGCIndiaHero from "./components/home-hero";
+import SiteSponsor from "./components/footer/site-sponsor";
 
 function App() {
   return (
     <div className="mx-auto">
+      <SiteBanner />
       <SiteHeader />
-      <div className="h-screen text-center bg-gray-100">content here</div>
+      <HomeSlider />
+      <EGCIndiaHero />
+      <SiteSponsor/>
       <SiteFooter />
     </div>
   );
--- src/components/header/site-header.tsx
+++ src/components/header/site-header.tsx
@@ -6,7 +6,7 @@ import { siteConfig } from "@/config/site-config";
 
 const SiteHeader = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background ">
       <nav className="h-16 md:h-20">
         <div className="h-full flex items-center justify-between max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center md:hidden">
